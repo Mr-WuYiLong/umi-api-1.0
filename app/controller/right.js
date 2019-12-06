00b823596841798a2cefd9d2ee9a9168e0114d06
yilongwu <1443622667@qq.com>
2019-12-06 17:53:14 +0800

    umi-api-1.0

--- app/controller/right.js
+++ app/controller/right.js
@@ -3,6 +3,10 @@
 const Controller = require('egg').Controller;
 class RightController extends Controller {
 
+  async index() {
+
+    const add = await this.app.enforcer();
+  }
 }
 
 module.exports = RightController;
